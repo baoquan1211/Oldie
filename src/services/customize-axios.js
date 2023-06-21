4b886c4a91138113c32cabe3c7fe6d52500278ae
Quách Bảo Quân <82823234+baoquan1211@users.noreply.github.com>
2023-06-21 23:18:06 +0700

    Update customize-axios.js
    fix backend link

--- src/services/customize-axios.js
+++ src/services/customize-axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://oldie-backend.onrender.com/",
+  baseURL: "https://oldie-backend.onrender.com",
 });
 
 instance.interceptors.response.use(
